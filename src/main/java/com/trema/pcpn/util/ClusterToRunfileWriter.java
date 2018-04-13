bac06849829ba9555b7f172a14b80534acd12e93
sumanta <sumanta@sumanta-Aspire-A515-51G>
2018-04-12 21:21:07 -0400

    bug fix for cluster qrels continue

--- src/main/java/com/trema/pcpn/util/ClusterToRunfileWriter.java
+++ src/main/java/com/trema/pcpn/util/ClusterToRunfileWriter.java
@@ -17,17 +17,37 @@ public class ClusterToRunfileWriter {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(p.getProperty("out-dir")+"/"+p.getProperty("cluster-out"))));
 			HashMap<String, ArrayList<ArrayList<String>>> resultPageClusters = (HashMap<String, ArrayList<ArrayList<String>>>)ois.readObject();
 			ois.close();
-			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(p.getProperty("out-dir")+"/"+p.getProperty("cluster-out")+"-cluster-run")));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(p.getProperty("out-dir")+"/"+p.getProperty("cluster-out")+"-cluster-runtemp")));
+			HashMap<String, ArrayList<String>> outrunFileMap = new HashMap<String, ArrayList<String>>();
 			for(String page:resultPageClusters.keySet()) {
 				for(ArrayList<String> cl:resultPageClusters.get(page)) {
 					for(String para1:cl) {
 						for(String para2:cl) {
 							if(!para1.equals(para2)) {
-								bw.write(para1+" Q0 "+para2+" 0 1 CLUSTER-RUN\n");
+								if(outrunFileMap.containsKey(para1) && !outrunFileMap.get(para1).contains(para2)) {
+									outrunFileMap.get(para1).add(para2);
+								}
+								else {
+									ArrayList<String> paralist = new ArrayList<String>();
+									paralist.add(para2);
+									outrunFileMap.put(para1, paralist);
+								}
 							}
 						}
 					}
 				}
+			}
+			for(String qp:outrunFileMap.keySet()) {
+				ArrayList<String> seenParaIDs = new ArrayList<String>();
+				for(String para:outrunFileMap.get(qp)) {
+					if(!seenParaIDs.contains(para)) {
+						if(qp.equalsIgnoreCase("f1f612e194dbc5964739677322529ca9a09ea429")||para.equalsIgnoreCase("f1f612e194dbc5964739677322529ca9a09ea429"))
+							System.out.println(qp+" Q0 "+para+" 0 1 CLUSTER-RUN");
+						else
+							bw.write(qp+" Q0 "+para+" 0 1 CLUSTER-RUN\n");
+						seenParaIDs.add(para);
+					}
+				}	
 			}
 			bw.close();
 		} catch (ClassNotFoundException | IOException e) {
